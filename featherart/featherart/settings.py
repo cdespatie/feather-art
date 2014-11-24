5b96a9c7d05a79d39a3c95b9bc2dd91a74aca807
Chris Despatie <christian_despatie@hotmail.com>
2014-11-23 21:56:25 -0500

    Setting up default templates and directories.

--- featherart/featherart/settings.py
+++ featherart/featherart/settings.py
@@ -97,8 +97,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+)
 
 # Media files (user uploaded)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Templates
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
