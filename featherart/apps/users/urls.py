503cbfa5518e92979cdb34349063b6b8ea2cd8e0
Chris Despatie <christian_despatie@hotmail.com>
2015-06-27 19:06:39 -0400

    Users can't get to the login page if they're already logged in.

--- featherart/apps/users/urls.py
+++ featherart/apps/users/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url, patterns
 from users.views import RegisterView, ActivateView
 from django.contrib.auth.views import login, logout
+from django.contrib.auth.decorators import user_passes_test
+
+# Redirect to homepage if user tries to access login page while already logged in.
+login_forbidden =  user_passes_test(lambda u: u.is_anonymous(), '/')
 
 urlpatterns = patterns('',
     url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^activate/$', ActivateView.as_view(), name='activate'),
-    url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+    url(r'^login/$', login_forbidden(login), {'template_name': 'users/login.html'}, name='login'),
     url(r'^logout/$', logout, {'template_name': 'users/logout.html', 'next_page':'/'}, name='logout')
 )
